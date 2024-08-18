9f46100ba3fb65e2b6268d7b943ed38ba800ca7f
Devora Aiello <vorabee@gmail.com>
2024-08-18 14:26:05 +0200

    update example

---
+++ readfile/readfile1.py
@@ -0,0 +1,25 @@
+# Open the file in read mode
+file = open('infile.txt', 'r')
+
+# Read the entire content of the file
+content = file.read()
+
+# Print the content
+print(content)
+
+ctr=0
+print("test forloop")
+# Print the lines
+for line in content:
+   print(line, end='')
+   ctr=ctr+1
+
+print("total=",ctr)
+
+#ifexample
+
+#switchexample
+
+# Close the file
+file.close()
+
