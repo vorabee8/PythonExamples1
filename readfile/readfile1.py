07d12a65f73cc09e06e145028ecdd824938d4c6b
Devora Aiello <vorabee@gmail.com>
2024-08-18 14:39:19 +0200

    update example

--- readfile/readfile1.py
+++ readfile/readfile1.py
@@ -23,3 +23,22 @@ print("total=",ctr)
 # Close the file
 file.close()
 
+# Let's try this again
+
+# Open the file in read mode
+file = open('infile.txt', 'r')
+
+# Read all lines from the file
+lines = file.readlines()
+
+ctr=0
+
+# Print the lines
+for line in lines:
+   print(line, end='')
+   ctr=ctr+1
+
+print("total =",ctr)
+
+# Close the file
+file.close()
