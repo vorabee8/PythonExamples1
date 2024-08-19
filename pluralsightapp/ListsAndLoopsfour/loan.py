beebff4c71588bacffc2c09f14bc2ea95c3237db
Devora Aiello <vorabee@gmail.com>
2024-08-19 22:02:06 +0200

    pluralsightappupdate

---
+++ pluralsightapp/ListsAndLoopsfour/loan.py
@@ -0,0 +1,26 @@
+
+# Get details of loan
+money_owed = float(input('How much money do you owe, in dollars?\n')) # $50,000
+apr = float(input('What is the annual percentage rate of the loan?\n')) # 3%
+payment = float(input('How much will youpay off each month in dollars?\n')) # $1,000
+months = int(input('How many months do you want to see the results for?\n')) #24
+
+monthly_rate = apr/100/12
+
+for i in range(months):	
+    # Calculate interest to pay
+    interest_paid = money_owed*monthly_rate
+
+    # Add in interest
+    money_owed = money_owed + interest_paid
+
+    if (money_owed - payment < 0):
+        print('The last payment is', money_owed)
+        print('You paid off the loan in', i+1, 'months')
+        break
+
+    # Make payment
+    money_owed = money_owed - payment
+
+    print('paid', payment, 'of which', interest_paid, 'was interest', end=' ')
+    print('Now I owe', money_owed)
